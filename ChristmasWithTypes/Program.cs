249d3f3530388f816cec00868cc40a03e7de4724
Brooke Elliott <brookeelliott711@gmail.com>
2023-12-09 13:54:22 -0700

    whiteboard

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -1,34 +1,32 @@
 ﻿using System;
 
-namespace ChristmasWithTypes
+namespace WhiteBoard
 {
     class Program
     {
-        static void Main(string[] args)
+        public static string WhiteBoard(int num)
         {
-            var xmas = new Christmas();
-
-            var xmasDay = Christmas.Day.Thursday;
-
-            //DONE set Santa's name to Kris Kringle
-            xmas.Santa = ("Kris Kringle");
-
-            //DONE Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{"Yeti SB140 LR Pink", "Weston Eclipse Splitboard", "Snow setup for my truck"}; 
-
-            //DONE Set the TreeHeight to 10
-            xmas.TreeHeight = 10;
-
-            Console.WriteLine($"This year christmas falls on {xmasDay} \n");
-            Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
-            Console.WriteLine("Here are the presents we would like:");
-
-            foreach (var present in xmas.Presents)
+            string answer = "";
+            if(num % 3 == 0 && num % 5 ==0)
             {
-                Console.WriteLine($"    {present}");
+                answer = "fizzbuzz";
+            }
+            else if(num % 3 == 0 && num % 5 != 0)
+            {
+                answer = "fizz";
+            }
+            else if(num % 5 == 0 && num % 3 != 0)
+            {
+                answer = "buzz";
             }
 
-            Console.WriteLine($"\n We like to call Santa, {xmas.Santa}");
+            return answer;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WhiteBoard(10));
+
         }
     }
 }
